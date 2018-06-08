579cd220b0fb934f26e78b2f87d05ef9eb5b45a2
evwhiz <evwhiz@gmail.com>
2018-06-07 23:13:04 -0400

    hello user

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World :-)"
+    user = {"username": "Evan"}
+    return """
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, """ + user["username"] + """!<h1>
+    </body>
+</html>"""
