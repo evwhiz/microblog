6ba60e32f3d65cbd3b0dadcc5ca4625e922bf5f2
evwhiz <evwhiz@gmail.com>
2018-07-06 14:33:52 -0400

    logged-in user templates

--- app/routes.py
+++ app/routes.py
@@ -36,7 +36,7 @@ def index():
             "body": "Throw the ball. Throw the ball. Was that the UPS truck?"
         }
     ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", posts=posts)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -51,7 +51,7 @@ def login():
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
-        if not next_page or url_parse(next_page).netlog != "":
+        if not next_page or url_parse(next_page).netloc != "":
             next_page = url_for("index")
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
